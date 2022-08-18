4eac59cb5044e6d86a9b9ca6b942262c37d981a6
lingulist <mattis.list@lingpy.org>
2022-08-18 14:09:41 +0200

    update new files

--- README.md
+++ README.md
@@ -15,21 +15,21 @@ Available online at https://numeralbank.clld.org
 ## Statistics
 
 
-![Glottolog: 99%](https://img.shields.io/badge/Glottolog-99%25-green.svg "Glottolog: 99%")
+![Glottolog: 99%](https://img.shields.io/badge/Glottolog-99%25-brightgreen.svg "Glottolog: 99%")
 ![Concepticon: 100%](https://img.shields.io/badge/Concepticon-100%25-brightgreen.svg "Concepticon: 100%")
 ![Source: 0%](https://img.shields.io/badge/Source-0%25-red.svg "Source: 0%")
 
-- **Varieties:** 4,371
-- **Concepts:** 153
-- **Lexemes:** 172,601
+- **Varieties:** 5,603
+- **Concepts:** 159
+- **Lexemes:** 211,895
 - **Sources:** 0
-- **Synonymy:** 1.07
+- **Synonymy:** 1.08
 
 ## Possible Improvements:
 
 
 
-- Entries missing sources: 172601/172601 (100.00%)
+- Entries missing sources: 211895/211895 (100.00%)
 
 ## CLDF Datasets
 
--- lexibank_numeralbank_analysed.py
+++ lexibank_numeralbank_analysed.py
@@ -22,8 +22,10 @@ from clldutils.misc import slug
 
 def find_system(language, relations):
     scores = {}
+    coverage = {}
     for system in relations:
         score, count = 0, 0
+        coverage[system] = 0
         for conceptA, conceptB in relations[system]:
             hit = False
             try:
@@ -35,19 +37,21 @@ def find_system(language, relations):
                 if hit:
                     score += 1
                 count += 1
+                coverage[system] += 1
             except KeyError:
                 pass
         if count:
             scores[system] = score / count
         else:
             scores[system] = 0
-    return scores
+    return scores, coverage
 
 
 @attr.s
 class CustomLanguage(Language):
     BestBase = attr.ib(default=None)
     Bases = attr.ib(default=None)
+    Base = attr.ib(default=None)
 
 
 
@@ -97,8 +101,8 @@ class Dataset(BaseDataset):
                         args.log.error('found neither main nor master branch')
                 repo.git.merge()
 
-        with self.raw_dir.temp_download(CLTS_2_1[0], 'ds.zip', log=args.log) as zipp:
-            zipfile.ZipFile(str(zipp)).extractall(self.raw_dir)
+        #with self.raw_dir.temp_download(CLTS_2_1[0], 'ds.zip', log=args.log) as zipp:
+        #    zipfile.ZipFile(str(zipp)).extractall(self.raw_dir)
 
 
     def cmd_makecldf(self, args):
@@ -124,32 +128,41 @@ class Dataset(BaseDataset):
             relations = json.load(f)
 
         for language in wl.languages:
-            scores = find_system(language, relations)
-            scoreS = " ".join(["{0}:{1:.2f}".format(k, v) for k, v in scores.items()])
-            bestSystem = [k for k, v in sorted(scores.items(), key=lambda x: x[1],
-                    reverse=True)][0]
-            if len(set(scores.values())) == 1 and list(scores.values())[0] == 0:
-                bestSystem = ""
-            if bestSystem and scores[bestSystem] < 0.1:
-                bestSystem = "Unknown"
-            if bestSystem:
-                args.log.info("{0} / {1}".format(language.name, bestSystem))
-                args.writer.add_language(
+            scores, coverage = find_system(language, relations)
+            # check for sufficient coverage
+            if len(set(coverage.values())) == 1 and list(coverage.values())[0] == 0:
+                args.log.info("Ignoring {0} with low coverage".format(language.name))
+            else:
+                if language.dataset == "numerals":
+                    real_base = language.data["Base"]
+                else:
+                    real_base = "unknown"
+                scoreS = " ".join(["{0}:{1:.2f}".format(k, v) for k, v in scores.items()])
+                bestSystem = [k for k, v in sorted(scores.items(), key=lambda x: x[1],
+                        reverse=True)][0]
+                if len(set(scores.values())) == 1 and list(scores.values())[0] == 0:
+                    bestSystem = ""
+                if bestSystem and scores[bestSystem] < 0.1:
+                    bestSystem = "Unknown"
+                if bestSystem:
+                    args.log.info("{0} / {1}".format(language.name, bestSystem))
+                    args.writer.add_language(
                         ID=language.id,
                         Name=language.name,
                         Glottocode=language.glottocode,
                         Latitude=language.latitude,
                         Longitude=language.longitude,
                         Bases=scoreS,
-                        BestBase=bestSystem
-                        )
-                for concept in language.concepts:
-                    for form in concept.forms:
-                        args.writer.add_form(
-                                Language_ID=language.id,
-                                Parameter_ID=slug(concept.id),
-                                Value=form.value,
-                                Form=form.form,
-                                Source=""
-                                )
+                        BestBase=bestSystem,
+                        Base=real_base
+                    )
+                    for concept in language.concepts:
+                        for form in concept.forms:
+                            args.writer.add_form(
+                                    Language_ID=language.id,
+                                    Parameter_ID=slug(concept.id),
+                                    Value=form.value,
+                                    Form=form.form,
+                                    Source=""
+                                    )
 
